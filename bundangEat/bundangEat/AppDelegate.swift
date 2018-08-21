19b552a2387d5e9320584e20fbbdfae2c5162d44
JeaSung Lee <wotjdzz1@naver.com>
2018-08-21 12:18:31 +0900

    init mac app

---
+++ bundangEat/bundangEat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  bundangEat
+//
+//  Created by JERCY on 2018. 8. 21..
+//  Copyright © 2018년 JERCY. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ bundangEat/bundangEat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  bundangEat
+//
+//  Created by JERCY on 2018. 8. 21..
+//  Copyright © 2018년 JERCY. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
